aa17d66b91453575c07cc034b0abcafc5b431195
Marissa Phul <mphul044@uottawa.ca>
2019-02-10 04:57:16 -0500

    new dot colours

--- dots.js
+++ dots.js
@@ -1,26 +1,25 @@
 function dots() {
-  // Some random colors
-  const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
+  const colors = ["#7ec13c", "#14cdfc", "#1B1B1B", "#dd8c00", "#f44274"];
 
-  const numBalls = 50;
-  const balls = [];
+  const numdots = 50;
+  const dots = [];
 
-  for (let i = 0; i < numBalls; i++) {
-    let ball = document.createElement("div");
-    ball.classList.add("ball");
-    ball.style.background = colors[Math.floor(Math.random() * colors.length)];
-    ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
-    ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
-    ball.style.transform = `scale(${Math.random()})`;
-    ball.style.width = `${Math.random()}em`;
-    ball.style.height = ball.style.width;
+  for (let i = 0; i < numdots; i++) {
+    let dot = document.createElement("div");
+    dot.classList.add("dot");
+    dot.style.background = colors[Math.floor(Math.random() * colors.length)];
+    dot.style.left = `${Math.floor(Math.random() * 100)}vw`;
+    dot.style.top = `${Math.floor(Math.random() * 100)}vh`;
+    dot.style.transform = `scale(${Math.random()})`;
+    dot.style.width = `${Math.random()}em`;
+    dot.style.height = dot.style.width;
     
-    balls.push(ball);
-    document.body.append(ball);
+    dots.push(dot);
+    document.body.append(dot);
   }
 
   // Keyframes
-  balls.forEach((el, i, ra) => {
+  dots.forEach((el, i, ra) => {
     let to = {
       x: Math.random() * (i % 2 === 0 ? -11 : 11),
       y: Math.random() * 12
