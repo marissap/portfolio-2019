311668e7a69d38159e54ddecb7f69b8f4f5219a8
Marissa Phul <mphul044@uottawa.ca>
2019-10-01 18:57:27 -0400

    added comments to js so i remember what i was thinking

--- dots.js
+++ dots.js
@@ -4,15 +4,16 @@ function dots() {
   const numdots = 50;
   const dots = [];
 
+  // setting up the css of the dots
   for (let i = 0; i < numdots; i++) {
     let dot = document.createElement("div");
     dot.classList.add("dot");
     dot.style.background = colors[Math.floor(Math.random() * colors.length)];
-    dot.style.left = `${Math.floor(Math.random() * 100)}vw`;
-    dot.style.top = `${Math.floor(Math.random() * 100)}vh`;
+    dot.style.left = `${Math.floor(Math.random() * 100)}vw`; // starting point on x-axis
+    dot.style.top = `${Math.floor(Math.random() * 100)}vh`; // starting point on y-axis
     dot.style.transform = `scale(${Math.random()})`;
-    dot.style.width = `${Math.random()}em`;
-    dot.style.height = dot.style.width;
+    dot.style.width = `${Math.random()}em`; // width of dot
+    dot.style.height = dot.style.width; // circle means width and height must match
     
     dots.push(dot);
     document.body.append(dot);
@@ -21,21 +22,21 @@ function dots() {
   // Keyframes
   dots.forEach((el, i, ra) => {
     let to = {
-      x: Math.random() * (i % 2 === 0 ? -11 : 11),
-      y: Math.random() * 12
+      x: Math.random() * (i % 2 === 0 ? -11 : 11), // distance to move along x-axis
+      y: Math.random() * 12 // distance to move along y-axis
     };
 
     let anim = el.animate(
       [
-        { transform: "translate(0, 0)" },
-        { transform: `translate(${to.x}rem, ${to.y}rem)` }
+        { transform: "translate(0, 0)" }, // starting position is from css values
+        { transform: `translate(${to.x}rem, ${to.y}rem)` } // use randomly calculated values to set distance of movement
       ],
       {
         duration: (Math.random() + 1) * 2000, // random duration
-        direction: "alternate",
+        direction: "alternate", // go back and forth
         fill: "both",
-        iterations: Infinity,
-        easing: "ease-in-out"
+        iterations: Infinity, // animation occurs forever
+        easing: "ease-in-out" // slow movement
       }
     );
   });
